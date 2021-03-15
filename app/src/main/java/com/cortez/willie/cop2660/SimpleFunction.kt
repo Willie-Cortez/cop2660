8b542ab0c185b0a07394f9f9173b805a7cb3dacd
Willie-Cortez <wcorte1@live.spcollege.edu>
2021-03-15 08:37:58 -0400

    added named arguments

--- app/src/main/java/com/cortez/willie/cop2660/SimpleFunction.kt
+++ app/src/main/java/com/cortez/willie/cop2660/SimpleFunction.kt
@@ -1,8 +1,29 @@
 package com.cortez.willie.cop2660
 
+import java.net.SocketOptions
+
 fun main(){
 
     fun start(): String="OK"
 
     println(start())
+
+
+// added 3/15/2021
+// named arguments
+//    fun joinToString(
+//        separator: String = ", ",
+//        prefix: String = "",
+//        postfix: String = "",
+//        /* ... */
+//    ): String
+
+    fun joinOptions(options: Collection<String>) =
+        options.joinToString(",","[","]")
+
+// trying to understand how it works
+    val chars = listOf<String>('a'.toString(), 'b'.toString(), 'c'.toString(), 'd'.toString(), 'e'.toString())
+    chars.joinToString()
+    println(chars)
+
 }
